2cfc2dfef30f1daf4e92639e6cd6b63f66fc72d7
ssairishith <ssairishith@gmail.com>
2026-04-28 00:06:22 +0530

    Update socket connection to use backend URL

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -2,6 +2,6 @@ import { io } from "socket.io-client";
 
 const BACKEND = import.meta.env.VITE_BACKEND_URL || "http://localhost:5000";
 
-const socket = io("/", { autoConnect: true });
+const socket = io(BACKEND, { autoConnect: true });
 
 export default socket;
